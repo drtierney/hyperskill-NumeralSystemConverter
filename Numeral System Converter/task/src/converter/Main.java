68a88ed3b2f529bbf6f4e83e6dde68007a560107
David Tierney <d.r.tierney@hotmail.co.uk>
2020-07-05 12:31:23 +0100

    Stage #2 completed
    Stage #2 Almost an octopus
    Learn the basics of working with Scanner class: teach the converter to determine the last digit of the octal number, converted from the decimal system.

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -1,7 +1,14 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("42 = 0b101010");
+        Scanner scanner = new Scanner(System.in);
+        int base = 8;
+        int num = scanner.nextInt();
+        int lastDigit = num % base;
+        System.out.println(lastDigit);
+
     }
 }
